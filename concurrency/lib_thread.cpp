1ac52fc41a3c388251f76ed226233965b30812cd
wjasonhuang <wjasonhuang@users.noreply.github.com>
2020-05-17 18:07:16 -0400

    thread

--- concurrency/lib_thread.cpp
+++ concurrency/lib_thread.cpp
@@ -19,9 +19,15 @@ this_thread::sleep_for()		blocks the calling thread during the span of time spec
 #include <thread>
 #include <vector>
 
-void threadFunction() {
+void threadFunction1() {
     std::this_thread::sleep_for(std::chrono::milliseconds(10)); // simulate work
-    std::cout << std::this_thread::get_id() << ": finished work in threadFunction\n";
+    std::cout << std::this_thread::get_id() << ": finished work in threadFunction1\n";
+}
+
+void threadFunction2(int id, int& ret) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(20)); // simulate work
+    std::cout << id << ": finished work in threadFunction2\n";
+    ret += 5;
 }
 
 class threadClass {
@@ -31,23 +37,24 @@ public:
 
 int main()
 {
+    int ret = 0;
+
     std::cout << "Number of hardware thread contexts: " << std::thread::hardware_concurrency() << "\n";
     std::vector<std::thread> threads;
 
     // create thread with function
-    std::thread t(threadFunction);
-    threads.push_back(std::move(t));
+    std::thread tf1(threadFunction1);
+    threads.push_back(std::move(tf1));
+    std::thread tf2(threadFunction2, 77, std::ref(ret));    // need to mark explicitly using std::ref when passing by reference
+    threads.push_back(std::move(tf2));
 
     // create thread with function objects
-    // std::thread t0(Vehicle()) would be interpreted as a function t0 with input Vehicle() and output std::thread
+    // std::thread t(Vehicle()) would be interpreted as a regular function named t
     std::thread t1( (threadClass()) );              // Add an extra pair of parantheses
     std::thread t2 = std::thread( threadClass() );  // Use copy initialization
     std::thread t3{ threadClass() };                // Use uniform initialization with braces
     threads.push_back(std::move(t1)); threads.push_back(std::move(t2)); threads.push_back(std::move(t3));
 
-    // do something in main()
-    std::this_thread::sleep_for(std::chrono::milliseconds(20)); // simulate work
-    std::cout << std::this_thread::get_id() << ": finished work in main\n";
-
     for (auto& it : threads) it.join();
+    std::cout << "ret = " << ret << "\n";
 }
