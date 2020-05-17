7bdfdf1fc45e9cfb6f45862e5aa01b70ae39e7f9
wjasonhuang <wjasonhuang@users.noreply.github.com>
2020-05-17 17:11:21 -0400

    updates

--- concurrency/lib_thread.cpp
+++ concurrency/lib_thread.cpp
@@ -3,10 +3,10 @@ http://www.cplusplus.com/reference/thread/thread/
 
 thread name(Fn, Args)
 
-thread::detach()			detaches the thread from the calling thread, allowing them to execute independently
-thread::join()				the function returns when the thread execution has completed
-thread::joinable()			returns whether the thread object is joinable
-thread::hardware_concurrency()		[static] returns the number of hardware thread contexts
+thread::detach()                detaches the thread from the calling thread, allowing them to execute independently
+thread::join()				    the function returns when the thread execution has completed
+thread::joinable()			    returns whether the thread object is joinable
+thread::hardware_concurrency()  [static] returns the number of hardware thread contexts
 
 namespace std::this_thread
 this_thread::get_id()			returns the thread id of the calling thread
@@ -17,33 +17,37 @@ this_thread::sleep_for()		blocks the calling thread during the span of time spec
 #include <chrono>
 #include <iostream>
 #include <thread>
+#include <vector>
 
-void threadFunction()
-{
-    std::this_thread::sleep_for(std::chrono::milliseconds(50)); // simulate work
-    std::cout << "Finished work 1 in thread\n";
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(50));
-    std::cout << "Finished work 2 in thread\n";
+void threadFunction() {
+    std::this_thread::sleep_for(std::chrono::milliseconds(10)); // simulate work
+    std::cout << std::this_thread::get_id() << ": finished work in threadFunction\n";
 }
+
+class threadClass {
+public:
+    void operator()() { std::cout << std::this_thread::get_id() << ": threadClass object has been created\n"; }
+};
 
 int main()
 {
-    // create thread
+    std::cout << "Number of hardware thread contexts: " << std::thread::hardware_concurrency() << "\n";
+    std::vector<std::thread> threads;
+
+    // create thread with function
     std::thread t(threadFunction);
+    threads.push_back(std::move(t));
+
+    // create thread with function objects
+    // std::thread t0(Vehicle()) would be interpreted as a function t0 with input Vehicle() and output std::thread
+    std::thread t1( (threadClass()) );              // Add an extra pair of parantheses
+    std::thread t2 = std::thread( threadClass() );  // Use copy initialization
+    std::thread t3{ threadClass() };                // Use uniform initialization with braces
+    threads.push_back(std::move(t1)); threads.push_back(std::move(t2)); threads.push_back(std::move(t3));
 
     // do something in main()
     std::this_thread::sleep_for(std::chrono::milliseconds(20)); // simulate work
-    std::cout << "Finished work 1 in main\n";
+    std::cout << std::this_thread::get_id() << ": finished work in main\n";
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(20));
-    std::cout << "Finished work 2 in main\n";
-
-    // wait for thread to finish
-    t.detach();
-    if (t.joinable()) t.join();
-
-    std::cout << "Main pausing for 200ms\n";
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));
-    return 0;
+    for (auto& it : threads) it.join();
 }
