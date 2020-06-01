91fbb1440fee411062f76e0b2a5698195f54b0af
wjasonhuang <wjasonhuang@users.noreply.github.com>
2020-05-31 20:23:02 -0400

    Create cp_template.cpp

---
+++ cp_template.cpp
@@ -0,0 +1,34 @@
+//#define DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+
+
+
+int solve() {
+    int n, ret;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> ;
+    }
+
+
+    return ret;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); // remove this line for interactive problems
+#ifdef DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+    int t = 1;
+    // cin >> t;
+    for (int i = 0; i < t; i++) {
+        cout << solve() << endl;
+    }
+}
