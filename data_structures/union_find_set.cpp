2c4f608690db2bc603b67d0a6d3d57511acce997
wjasonhuang <wjasonhuang@users.noreply.github.com>
2020-08-16 14:19:10 -0400

    Update union_find_set.cpp

--- data_structures/union_find_set.cpp
+++ data_structures/union_find_set.cpp
@@ -1,25 +1,28 @@
+#include <numeric>
+#include <vector>
+using namespace std;
+
 class UnionFindSet {
 public:
+    vector<int> p, rk;
+
     UnionFindSet(int n) {
         rk = vector<int>(n, 0);
-        p = vector<int>(n);
-        for (int i = 0; i < n; i++) p[i] = i;
+        p.resize(n);
+        iota(p.begin(), p.end(), 0);
     }
 
     int find(int v) {
-        if (p[v] != v) p[v] = find(p[v]);
-        return p[v];
-    }
-    
-    bool merge(int v1, int v2) {
-        int p1 = find(v1), p2 = find(v2);
-        if (p1 == p2) return true;
-        if (rk[p1] < rk[p2]) swap(p1, p2);
-        p[p2] = p1;
-        rk[p1] += rk[p1] == rk[p2];
-        return false;
+        return (p[v] == v ? v : p[v] = find(p[v]));
     }
 
-private:
-    vector<int> p, rk;
+    bool merge(int v1, int v2) {
+        v1 = find(v1);
+        v2 = find(v2);
+        if (v1 == v2) return true;
+        if (rk[v1] < rk[v2]) swap(v1, v2);
+        p[v2] = v1;
+        rk[v1] += rk[v1] == rk[v2];
+        return false;
+    }
 };
