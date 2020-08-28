a2104be09a95b4410144ed850c335a33c3a0a39f
wjasonhuang <wjasonhuang@users.noreply.github.com>
2020-08-27 23:43:18 -0400

    Update union_find_set.cpp

--- data_structures/union_find_set.cpp
+++ data_structures/union_find_set.cpp
@@ -1,28 +1,21 @@
 #include <algorithm>    // std::swap
-#include <numeric>      // std:iota
 #include <vector>       // std:vector
 using std::vector;
 using std::swap;
 
 class UnionFindSet {
 public:
-    vector<int> p, rk;
+    vector<int> p, c;
 
-    UnionFindSet(int n) {
-        rk = vector<int>(n, 0);
-        p.resize(n);
-        iota(p.begin(), p.end(), 0);
-    }
-
-    int find(int v) { return (p[v] == v ? v : p[v] = find(p[v])); }
-
+    UnionFindSet(int n) { p = vector<int>(n, -1); c = vector<int>(n, 1); }
+    
+    int find(int v) { return (p[v] == -1 ? v : p[v] = find(p[v])); }
+    
     bool merge(int v1, int v2) {
-        v1 = find(v1);
-        v2 = find(v2);
+        v1 = find(v1); v2 = find(v2);
         if (v1 == v2) return true;
-        if (rk[v1] < rk[v2]) swap(v1, v2);
-        p[v2] = v1;
-        rk[v1] += rk[v1] == rk[v2];
+        if (c[v1] < c[v2]) swap(v1, v2);
+        p[v2] = v1; c[v1] += c[v2];
         return false;
     }
 };
